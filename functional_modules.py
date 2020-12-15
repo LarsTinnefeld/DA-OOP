8680ec2706d4f80040a947161a72df79137d0cb2
Lars Tinnefeld <50445480+LarsTinnefeld@users.noreply.github.com>
2020-12-14 19:12:00 -0500

    Update functional_modules.py

--- functional_modules.py
+++ functional_modules.py
@@ -3,7 +3,9 @@ from general_wh_processes import Functional_module
 
 
 class Gtp_module(Functional_module):
-    ''' The GTP_module class represents a pick environment of an automated pick process according the goods to picker principle.
+    '''
+    The GTP_module class represents a pick environment of an
+    automated pick process according the goods to picker principle.
 
         Inputs:
         - required order lines per hour
@@ -21,16 +23,18 @@ class Gtp_module(Functional_module):
             self, req_units_per_hour, req_lines_per_hour)
 
         # Following are module-specific constants
-        self.cost_per_unit = 500000  # Euros per station
-        self.capacity_per_unit = 600  # order lines per picker (station)
+        self.cost_per_unit = 100000  # per station
+        self.capacity_per_unit = 500  # order lines per picker (station)
 
     def sizer(self):
-        ''' The sizer is calculating the required amount of units to fulfill the needed capacity.
+        '''
+        The sizer is calculating the required amount of units to fulfill the needed capacity.
 
         Inputs: None
 
         Outputs: Amount of needed GTP units
         '''
+        
         self.module_cluster_size = math.ceil(
             self.req_lines_per_hour / self.capacity_per_unit)
         return self.module_cluster_size
@@ -44,11 +48,20 @@ class Gtp_module(Functional_module):
     def __repr__(self):
         self.sizer()
         self.calculate_cost()
-        return "Functional module: Goods to picker\nRequirement: {} order line per hour\nNeeded units: {}\nTotal cost: {}".format(self.req_lines_per_hour, self.module_cluster_size, self.total_cost)
+        return "Functional module: Goods to picker\n \
+            Requirement: {} order line per hour\n \
+            Needed units: {}\n \
+            Total cost: {}".format(
+                self.req_lines_per_hour,
+                self.module_cluster_size,
+                self.total_cost
+                )
 
 
-class Manual_pick_zone():
-    ''' The Manual_pick_zone class represents a pick environment of a manual pick process according the picker to goods principle.
+class Manual_pick_zone(Functional_module):
+    '''
+    The Manual_pick_zone class represents a pick environment of
+    a manual pick process according the picker to goods principle.
 
         Inputs:
         - required order lines per hour
@@ -59,3 +72,8 @@ class Manual_pick_zone():
         - Investment price in Euros
         - FTE requirement in FTEs per shift
     '''
+
+    def __init__(self, req_units_per_hour, req_lines_per_hour):
+
+        Functional_module.__init__(
+            self, req_units_per_hour, req_lines_per_hour)
